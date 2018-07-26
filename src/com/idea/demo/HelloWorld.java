3a01ae5e4c74d263bc67bafad50de5280a5a184a
wangbo <wangbosxly@126.com>
2018-07-26 21:45:30 +0800

    Initial commit

---
+++ src/com/idea/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.idea.demo;
+
+/**
+ * Created by wangbo on 2018-07-26.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
